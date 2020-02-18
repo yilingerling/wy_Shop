2d797909997682f3cfff0e8bcd5935483e1bb929
赵聪 <18633604593@163.com>
2020-02-18 16:29:21 +0800

    完成底部导航栏

--- src/components/FooterGuide/FooterGuide.vue
+++ src/components/FooterGuide/FooterGuide.vue
@@ -1,18 +1,23 @@
 <template>
   <div id="footerContainer">
       <div @click="goPath('/home')" class="guideItem">
-          <span>首页</span>
+        <span class="iconfont icon-shouye"></span>
+          <span class="txt">首页</span>
       </div>
       <div @click="goPath('/classification')" class="guideItem">
+      <span class="iconfont icon-leimupinleifenleileibie1"></span>
           <span>分类</span>
       </div>
       <div @click="goPath('/good')" class="guideItem">
+      <span class="iconfont icon-zhidemai2"></span>
           <span>值得买</span>
       </div>
       <div @click="goPath('/commodity')" class="guideItem">
+      <span class="iconfont icon-che"></span>
           <span>购物车</span>
       </div>
       <div @click="goPath('/personal')" class="guideItem">
+      <span class="iconfont icon-renwu"></span>
           <span>个人</span>
       </div>
   </div>
@@ -31,11 +36,21 @@
 
 <style scoped lang="stylus">
     #footerContainer 
+        width 100%
+        height 98px
+        line-height 42px
         display flex
+        border-top 1px solid gray
+        position fixed
+        left 0
+        bottom 0
         .guideItem
-            width 20%
-            height 48px
+            display flex
+            flex-direction column
+            width 20%       
             text-align center
-            
+            .iconfont
+                padding-top 10px
+                font-size 42px
  
 </style>
--- vue.cnfig.js
+++ vue.cnfig.js
@@ -1,4 +1,22 @@
-moudle.exports = {
+const px2rem = require('postcss-px2rem')
+// 配置postcs-px2rem
+const postcss = px2rem({
+  remUnit: 75   // 设计稿10等分之后的值
+})
+
+
+
+module.exports = {
   runtimeCompiler: true,
-  lintOnSave: false,//关闭enlint语法检查  
+  lintOnSave: false, // 关闭enlint语法检查
+  css: { // 添加postcss配置
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          postcss
+        ]
+      }
+    }
+  }
 }
+
